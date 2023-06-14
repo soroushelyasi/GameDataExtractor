7358962bc9cb1b196ab659679d82c28dfa9ae99e
ElyosCaesar <ElyosCaesar@DESKTOP-7AUGHBB>
2023-06-14 11:12:16 +0330

    next commit3

--- GameDataExtractor/Program.cs
+++ GameDataExtractor/Program.cs
@@ -131,7 +131,7 @@ namespace GameDataExtractor
             {
                 if (index == 0)
                 {
-                    return "S";//steady
+                    return "S";
                 }
 
                 if (elem == moveList[index - 1])
